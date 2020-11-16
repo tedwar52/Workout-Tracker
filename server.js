1e23ab5de0d8b79ab9b4013138664f2eea57ba64
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-11-15 21:41:59 -0500

    html routes complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const path = require("path");
 
 const db = require("./models");
 //imports models
@@ -17,10 +18,24 @@ app.use(express.static("public")); /*might change "public"*/
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true});
 
-//--------------Routes----------------------------------
+//--------------ROUTES----------------------------------
 
+//-----html routes-----------
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+//-----api routes------------
 /* "new workout" */
-app.post("/exercise", ({body}, res) => {
+app.post("/api/exercise", ({body}, res) => {
     db.Exercise.create(body)
     .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true}))
     .then(dbWorkout => {
